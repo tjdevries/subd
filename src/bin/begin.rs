f4c2bc286de92e528a214d95b906b3ee2519e8a0
David Begin <davidmichaelbe@gmail.com>
2023-03-12 22:14:38 -0700

    Turning off Twitter

--- src/bin/begin.rs
+++ src/bin/begin.rs
@@ -184,6 +184,13 @@ impl EventHandler for SoundHandler {
                 }
                 _ => continue,
             };
+
+            // fmt.Sprintf("Hello: %s", msg.contents);
+            let m = format!("\n\tSound Handler: {}", msg.contents);
+            println!("{:?}", m);
+
+            println!("Roles {:?}", msg.roles);
+
             let spoken_string = msg.contents.clone();
             let voice_text = msg.contents.to_string();
             let speech_bubble_text = uberduck::chop_text(spoken_string);
@@ -202,10 +209,14 @@ impl EventHandler for SoundHandler {
             let state =
                 twitch_stream_state::get_twitch_state(&self.pool).await?;
 
+            // Are we failing here???
+
             let mut character = Character {
                 ..Default::default()
             };
 
+            // See if I'm none of these!!!!
+            //
             // This is all about how to respond to messages from various
             // types of users
             if msg.roles.is_twitch_staff() {
@@ -219,10 +230,13 @@ impl EventHandler for SoundHandler {
             } else if msg.roles.is_twitch_sub() {
                 character.voice = Some(stream_character.voice.clone());
             } else if !state.sub_only_tts {
+           
                 // This is what everyone get's to speak with
                 // if we are allowing non-subs to speak
                 character.voice = Some(stream_character.voice.clone());
             }
+            
+            println!("WE MADE IT!"); 
 
             // If we have a voice assigned, then we fire off an UberDuck Request
             match character.voice {
@@ -240,7 +254,7 @@ impl EventHandler for SoundHandler {
 
             // If we have the implicit_soundeffects enabled
             // we go past this!
-            if !state.implicit_soundeffects {
+            if state.implicit_soundeffects {
                 continue;
             }
 
@@ -252,7 +266,6 @@ impl EventHandler for SoundHandler {
 
             let text_source =
                 server::obs::SOUNDBOARD_TEXT_SOURCE_NAME.to_string();
-
             for word in splitmsg {
                 let sanitized_word = word.as_str().to_lowercase();
                 let full_name = format!("./MP3s/{}.mp3", sanitized_word);
@@ -432,190 +445,11 @@ async fn main() -> Result<()> {
     event_loop.push(SourceVisibilityHandler { obs_client });
 
     println!("\n\n\t\tLet's Start this Loop Up!");
-    let _ = main2().await;
+    // We aren't running!!!!
+    // let _ = main().await;
     event_loop.run().await?;
 
     Ok(())
 }
 
-use twitter_v2::api_result::ApiResponse;
-use twitter_v2::authorization::{BearerToken, Oauth2Token};
-use twitter_v2::id::NumericId;
-use twitter_v2::query::{
-    SpaceExpansion, SpaceField, TopicField, TweetField, UserField,
-};
-use twitter_v2::Space;
-use twitter_v2::TwitterApi;
-
-// |          --------- ^^^^^^^^^^ the trait `IntoNumericId` is not implemented for `&str`
-
-async fn main2() -> Result<()> {
-    let otherside_guild_id = NumericId::new(1521585633445122048);
-    // let other_guild_tweet = 1607890390811840512;
-    let phil_tweet = NumericId::new(1608588236452167681);
-
-    let phil_id = NumericId::new(34440817);
-
-    let auth =
-        BearerToken::new(std::env::var("TWITTER_APP_BEARER_TOKEN").unwrap());
-
-    // let tweet = TwitterApi::new(auth)
-    //     .get_tweet(phil_tweet)
-    //     .tweet_fields([TweetField::AuthorId, TweetField::CreatedAt])
-    //     .send()
-    //     .await?
-    //     .into_data()
-    //     .expect("this tweet should exist");
-    // println!("{:?}", tweet);
-
-    // let auth =
-    //     BearerToken::new(std::env::var("TWITTER_APP_BEARER_TOKEN").unwrap());
-
-    // So this ID doesn't seem right
-    // We need to figure out a different one
-    // println!("{:?}", space);
-    //
-    let auth =
-        BearerToken::new(std::env::var("TWITTER_APP_BEARER_TOKEN").unwrap());
-
-    // /(invited_user_ids, speaker_ids, creator_id, host_ids, topics_ids)/ expansions
-    // 1ypKddPokoaKW
-    // let space = TwitterApi::new(auth)
-    //     .get_spaces_by_creator_ids([otherside_guild_id])
-    //     // So these aren't working how I expected
-    //     // I thought doing this could show the topics
-    //     .topic_fields([
-    //         TopicField::Id,
-    //         TopicField::Name,
-    //         TopicField::Description,
-    //     ])
-    //     .user_fields([
-    //         UserField::CreatedAt,
-    //         UserField::Description,
-    //         UserField::Id,
-    //         UserField::Name,
-    //         // UserField::PinnedTweetId,
-    //         // UserField::ProfileImageUrl,
-    //         // UserField::PublicMetrics,
-    //         UserField::Username,
-    //         UserField::Verified,
-    //         // Entities,
-    //         // Location,
-    //         // Protected,
-    //         // Url,
-    //         // Withheld,
-    //     ])
-    //     .expansions([
-    //         SpaceExpansion::HostIds,
-    //         // SpaceExpansion::InvitedUserIds,
-    //         SpaceExpansion::SpeakerIds,
-    //         SpaceExpansion::CreatorId,
-    //     ])
-    //     .space_fields([
-    //         SpaceField::HostIds,
-    //         SpaceField::CreatedAt,
-    //         SpaceField::CreatorId,
-    //         SpaceField::Id,
-    //         // SpaceField::Lang,
-    //         // SpaceField::InvitedUserIds,
-    //         SpaceField::ParticipantCount,
-    //         SpaceField::SpeakerIds,
-    //         SpaceField::StartedAt,
-    //         SpaceField::EndedAt,
-    //         // SpaceField::SubscriberCount,
-    //         SpaceField::TopicIds,
-    //         SpaceField::State,
-    //         SpaceField::Title,
-    //         // SpaceField::UpdatedAt,
-    //         SpaceField::ScheduledStart,
-    //         // SpaceField::IsTicketed,
-    //     ])
-    //     .send()
-    //     .await?
-    //     .into_data()
-    //     .expect("Space Not Found");
-    // println!("\n\n\t\tSpace: {:?}", space);
-
-    let auth =
-        BearerToken::new(std::env::var("TWITTER_APP_BEARER_TOKEN").unwrap());
-
-    let space = TwitterApi::new(auth)
-        .get_space("1ypKddPokoaKW")
-        .topic_fields([
-            TopicField::Id,
-            TopicField::Name,
-            TopicField::Description,
-        ])
-        .user_fields([
-            UserField::CreatedAt,
-            UserField::Description,
-            UserField::Id,
-            UserField::Name,
-            UserField::PinnedTweetId,
-            UserField::ProfileImageUrl,
-            UserField::PublicMetrics,
-            UserField::Username,
-            UserField::Verified,
-            // Entities,
-            // Location,
-            // Protected,
-            // Url,
-            // Withheld,
-        ])
-        .expansions([
-            SpaceExpansion::HostIds,
-            SpaceExpansion::SpeakerIds,
-            SpaceExpansion::CreatorId,
-            // SpaceExpansion::InvitedUserIds,
-        ])
-        .space_fields([
-            SpaceField::HostIds,
-            SpaceField::CreatedAt,
-            SpaceField::CreatorId,
-            SpaceField::Id,
-            SpaceField::ParticipantCount,
-            SpaceField::SpeakerIds,
-            SpaceField::StartedAt,
-            SpaceField::EndedAt,
-            SpaceField::TopicIds,
-            SpaceField::State,
-            SpaceField::Title,
-            SpaceField::ScheduledStart,
-            // SpaceField::Lang,
-            // SpaceField::InvitedUserIds,
-            // SpaceField::SubscriberCount,
-            // SpaceField::UpdatedAt,
-            // SpaceField::IsTicketed,
-        ])
-        .send()
-        .await?;
-
-    let data = space.data().expect("Expected space data");
-    println!("{:?}", data);
-
-    println!("Looking for Tweets for Space");
-
-    let stored_oauth2_token =
-        std::fs::read_to_string("./.oauth2_token.json").unwrap();
-    let auth: Oauth2Token = serde_json::from_str(&stored_oauth2_token)?;
-
-    let tweets = TwitterApi::new(auth)
-        .get_space_tweets("1djGXlXZogOGZ")
-        .send()
-        .await?;
-
-    let tweet_data = tweets.data().expect("Couldn't find tweets");
-    println!("\n\t\tTweets:{:?}", tweet_data);
-    // O-AUTH
-    // So I don't know how I should get a stored oauth2_token here
-    // So how do we make a stored Oauthtoken here
-    Ok(())
-}
-
-// async fn find_includes(
-//     space: &ApiResponse<BearerToken, Space, ()>,
-// ) -> Result<()> {
-//     // Can we return the space tho???
-//     let includes = space.into_includes().expect("expected includes");
-//     Ok(())
-// }
+// so We can seperate all this!
---
+++ src/bin/twitter.rs
@@ -0,0 +1,181 @@
+use twitter_v2::api_result::ApiResponse;
+use twitter_v2::authorization::{BearerToken, Oauth2Token};
+use twitter_v2::id::NumericId;
+use twitter_v2::query::{
+    SpaceExpansion, SpaceField, TopicField, TweetField, UserField,
+};
+use twitter_v2::Space;
+use twitter_v2::TwitterApi;
+
+// |          --------- ^^^^^^^^^^ the trait `IntoNumericId` is not implemented for `&str`
+
+async fn main() -> Result<()> {
+    let otherside_guild_id = NumericId::new(1521585633445122048);
+    // let other_guild_tweet = 1607890390811840512;
+    let phil_tweet = NumericId::new(1608588236452167681);
+
+    let phil_id = NumericId::new(34440817);
+
+    let auth =
+        BearerToken::new(std::env::var("TWITTER_APP_BEARER_TOKEN").unwrap());
+
+    // let tweet = TwitterApi::new(auth)
+    //     .get_tweet(phil_tweet)
+    //     .tweet_fields([TweetField::AuthorId, TweetField::CreatedAt])
+    //     .send()
+    //     .await?
+    //     .into_data()
+    //     .expect("this tweet should exist");
+    // println!("{:?}", tweet);
+
+    // let auth =
+    //     BearerToken::new(std::env::var("TWITTER_APP_BEARER_TOKEN").unwrap());
+
+    // So this ID doesn't seem right
+    // We need to figure out a different one
+    // println!("{:?}", space);
+    //
+    let auth =
+        BearerToken::new(std::env::var("TWITTER_APP_BEARER_TOKEN").unwrap());
+
+    // /(invited_user_ids, speaker_ids, creator_id, host_ids, topics_ids)/ expansions
+    // 1ypKddPokoaKW
+    // let space = TwitterApi::new(auth)
+    //     .get_spaces_by_creator_ids([otherside_guild_id])
+    //     // So these aren't working how I expected
+    //     // I thought doing this could show the topics
+    //     .topic_fields([
+    //         TopicField::Id,
+    //         TopicField::Name,
+    //         TopicField::Description,
+    //     ])
+    //     .user_fields([
+    //         UserField::CreatedAt,
+    //         UserField::Description,
+    //         UserField::Id,
+    //         UserField::Name,
+    //         // UserField::PinnedTweetId,
+    //         // UserField::ProfileImageUrl,
+    //         // UserField::PublicMetrics,
+    //         UserField::Username,
+    //         UserField::Verified,
+    //         // Entities,
+    //         // Location,
+    //         // Protected,
+    //         // Url,
+    //         // Withheld,
+    //     ])
+    //     .expansions([
+    //         SpaceExpansion::HostIds,
+    //         // SpaceExpansion::InvitedUserIds,
+    //         SpaceExpansion::SpeakerIds,
+    //         SpaceExpansion::CreatorId,
+    //     ])
+    //     .space_fields([
+    //         SpaceField::HostIds,
+    //         SpaceField::CreatedAt,
+    //         SpaceField::CreatorId,
+    //         SpaceField::Id,
+    //         // SpaceField::Lang,
+    //         // SpaceField::InvitedUserIds,
+    //         SpaceField::ParticipantCount,
+    //         SpaceField::SpeakerIds,
+    //         SpaceField::StartedAt,
+    //         SpaceField::EndedAt,
+    //         // SpaceField::SubscriberCount,
+    //         SpaceField::TopicIds,
+    //         SpaceField::State,
+    //         SpaceField::Title,
+    //         // SpaceField::UpdatedAt,
+    //         SpaceField::ScheduledStart,
+    //         // SpaceField::IsTicketed,
+    //     ])
+    //     .send()
+    //     .await?
+    //     .into_data()
+    //     .expect("Space Not Found");
+    // println!("\n\n\t\tSpace: {:?}", space);
+
+    let auth =
+        BearerToken::new(std::env::var("TWITTER_APP_BEARER_TOKEN").unwrap());
+
+    let space = TwitterApi::new(auth)
+        .get_space("1ypKddPokoaKW")
+        .topic_fields([
+            TopicField::Id,
+            TopicField::Name,
+            TopicField::Description,
+        ])
+        .user_fields([
+            UserField::CreatedAt,
+            UserField::Description,
+            UserField::Id,
+            UserField::Name,
+            UserField::PinnedTweetId,
+            UserField::ProfileImageUrl,
+            UserField::PublicMetrics,
+            UserField::Username,
+            UserField::Verified,
+            // Entities,
+            // Location,
+            // Protected,
+            // Url,
+            // Withheld,
+        ])
+        .expansions([
+            SpaceExpansion::HostIds,
+            SpaceExpansion::SpeakerIds,
+            SpaceExpansion::CreatorId,
+            // SpaceExpansion::InvitedUserIds,
+        ])
+        .space_fields([
+            SpaceField::HostIds,
+            SpaceField::CreatedAt,
+            SpaceField::CreatorId,
+            SpaceField::Id,
+            SpaceField::ParticipantCount,
+            SpaceField::SpeakerIds,
+            SpaceField::StartedAt,
+            SpaceField::EndedAt,
+            SpaceField::TopicIds,
+            SpaceField::State,
+            SpaceField::Title,
+            SpaceField::ScheduledStart,
+            // SpaceField::Lang,
+            // SpaceField::InvitedUserIds,
+            // SpaceField::SubscriberCount,
+            // SpaceField::UpdatedAt,
+            // SpaceField::IsTicketed,
+        ])
+        .send()
+        .await?;
+
+    let data = space.data().expect("Expected space data");
+    println!("{:?}", data);
+
+    println!("Looking for Tweets for Space");
+
+    let stored_oauth2_token =
+        std::fs::read_to_string("./.oauth2_token.json").unwrap();
+    let auth: Oauth2Token = serde_json::from_str(&stored_oauth2_token)?;
+
+    let tweets = TwitterApi::new(auth)
+        .get_space_tweets("1djGXlXZogOGZ")
+        .send()
+        .await?;
+
+    let tweet_data = tweets.data().expect("Couldn't find tweets");
+    println!("\n\t\tTweets:{:?}", tweet_data);
+    // O-AUTH
+    // So I don't know how I should get a stored oauth2_token here
+    // So how do we make a stored Oauthtoken here
+    Ok(())
+}
+
+// async fn find_includes(
+//     space: &ApiResponse<BearerToken, Space, ()>,
+// ) -> Result<()> {
+//     // Can we return the space tho???
+//     let includes = space.into_includes().expect("expected includes");
+//     Ok(())
+// }
