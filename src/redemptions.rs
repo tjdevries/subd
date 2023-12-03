832bcccd58c56f71723bf95d83282ce3f399a16a
David Begin <davidmichaelbe@gmail.com>
2023-12-02 23:26:04 -0800

    Getting closer

--- src/redemptions.rs
+++ src/redemptions.rs
@@ -1,40 +1,46 @@
 use anyhow::Result;
 use sqlx::PgPool;
+use sqlx::types::Uuid;
 use subd_macros::database_model;
 
-// #[database_model]
-// pub mod twitch_stream_state {
-//     use super::*;
-//
-//     pub struct Model {
-//         pub sub_only_tts: bool,
-//         pub explicit_soundeffects: bool,
-//         pub implicit_soundeffects: bool,
-//         pub global_voice: bool,
-//     }
-// }
-//
-// impl twitch_stream_state::Model {
-//     #[allow(dead_code)]
-//
-//     pub async fn save(self, pool: &PgPool) -> Result<Self> {
-//         Ok(sqlx::query_as!(
-//             Self,
-//             r#"
-//             INSERT INTO twitch_stream_state
-//             (sub_only_tts, explicit_soundeffects, implicit_soundeffects, global_voice)
-//             VALUES ( $1, $2, $3, $4)
-//             RETURNING sub_only_tts, explicit_soundeffects, implicit_soundeffects, global_voice
-//         "#,
-//             true,
-//             true,
-//             true,
-//             false,
-//         )
-//         .fetch_one(pool)
-//         .await?)
-//     }
-// }
+#[database_model]
+pub mod redemptions {
+    use super::*;
+
+    pub struct Model {
+        pub title: String,
+        pub cost: i32,
+        pub user_name: String,
+        pub reward_id: Uuid,
+        // This might need to be text
+        // optional might FUCKING US 
+        pub user_input: Option<String>,
+    }
+}
+
+impl redemptions::Model {
+    #[allow(dead_code)]
+
+    pub async fn save(self, pool: &PgPool) -> Result<Self> {
+        
+        Ok(sqlx::query_as!(
+            Self,
+            r#"
+            INSERT INTO redemptions 
+            (title, cost, user_name, reward_id, user_input)
+            VALUES ( $1, $2, $3, $4, $5)
+            RETURNING title, cost, user_name, reward_id, user_input
+        "#,
+            self.title,
+            self.cost,
+            self.user_name,
+            self.reward_id,
+            self.user_input
+        )
+        .fetch_one(pool)
+        .await?)
+    }
+}
 //
 // pub async fn turn_off_global_voice(pool: &PgPool) -> Result<()> {
 //     let _res =
