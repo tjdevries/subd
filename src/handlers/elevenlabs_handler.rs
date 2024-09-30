9cf715b056ed6a0ef2212195615eb8b9fb36caa0
beginbot <beginbot@beginbots-MBP.home>
2024-09-29 21:44:51 -0700

    Wow

--- src/handlers/elevenlabs_handler.rs
+++ src/handlers/elevenlabs_handler.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use anyhow::Result;
 use async_trait::async_trait;
 use colored::Colorize;
@@ -8,14 +9,11 @@ use elevenlabs_api::{
 use events::EventHandler;
 use obws::Client as OBSClient;
 use rodio::*;
-use std::fs::File;
-use std::io::BufReader;
-use std::sync::Arc;
+use std::{fs::File, io::BufReader};
 use stream_character;
 use subd_elevenlabs;
-use subd_types::Event;
+use subd_types::{ElevenLabsRequest, Event};
 use tokio::sync::broadcast;
-use tokio::sync::Mutex;
 use twitch_irc::{
     login::StaticLoginCredentials, SecureTCPTransport, TwitchIRCClient,
 };
@@ -39,41 +37,48 @@ impl EventHandler for ElevenLabsHandler {
     ) -> Result<()> {
         println!("{}", "Kicking off ElevenLabsHandler".yellow());
 
-        let twitch_client = Arc::new(Mutex::new(self.twitch_client));
-        let clone_twitch_client = twitch_client.clone();
-        let _locked_client = clone_twitch_client.lock().await;
-
-        let obs_client = Arc::new(Mutex::new(self.obs_client));
-        let obs_client_clone = obs_client.clone();
-        let _locked_obs_client = obs_client_clone.lock().await;
-
         loop {
-            // This feels dumb
-            let default_global_voice = "ethan".to_string();
             let event = rx.recv().await?;
 
-            let msg = match event {
-                Event::ElevenLabsRequest(msg) => msg,
-                _ => continue,
-            };
+            if let Event::ElevenLabsRequest(msg) = event {
+                self.process_elevenlabs_request(msg).await?;
+            }
+        }
+    }
+}
 
-            let ch = match msg.message.chars().next() {
-                Some(ch) => ch,
-                None => {
-                    continue;
-                }
-            };
-            if ch == '!' || ch == '@' {
-                continue;
-            };
+impl ElevenLabsHandler {
+    async fn process_elevenlabs_request(
+        &self,
+        msg: ElevenLabsRequest,
+    ) -> Result<()> {
+        if self.should_skip_message(&msg.message) {
+            return Ok(());
+        }
 
-            let pool_clone = self.pool.clone();
+        let final_voice = self.determine_final_voice(&msg).await?;
+        let chat_message =
+            subd_elevenlabs::sanitize_chat_message(msg.message.clone());
+        let audio_path = self
+            .process_audio(&final_voice, &chat_message, &msg)
+            .await?;
 
-            let twitch_state = async {
-                twitch_stream_state::get_twitch_state(&pool_clone).await
-            };
+        self.play_audio(&audio_path, &final_voice);
+        Ok(())
+    }
 
-            let is_global_voice_enabled = match twitch_state.await {
+    fn should_skip_message(&self, message: &str) -> bool {
+        matches!(message.chars().next(), Some('!') | Some('@'))
+    }
+
+    async fn determine_final_voice(
+        &self,
+        msg: &ElevenLabsRequest,
+    ) -> Result<String> {
+        let default_global_voice = "ethan".to_string();
+
+        let is_global_voice_enabled =
+            match twitch_stream_state::get_twitch_state(&self.pool).await {
                 Ok(state) => state.global_voice,
                 Err(err) => {
                     eprintln!("Error fetching twitch_stream_state: {:?}", err);
@@ -81,134 +86,133 @@ impl EventHandler for ElevenLabsHandler {
                 }
             };
 
-            let global_voice = stream_character::get_voice_from_username(
-                &self.pool, "beginbot",
-            )
-            .await
-            .unwrap_or(default_global_voice);
+        let global_voice =
+            stream_character::get_voice_from_username(&self.pool, "beginbot")
+                .await
+                .unwrap_or_else(|_| default_global_voice.clone());
 
-            let user_voice_opt = stream_character::get_voice_from_username(
-                &self.pool,
-                msg.username.clone().as_str(),
-            )
-            .await;
+        let user_voice = stream_character::get_voice_from_username(
+            &self.pool,
+            &msg.username,
+        )
+        .await
+        .unwrap_or_else(|_| global_voice.clone());
 
-            // We sometimes pass a voice with the message, for various effects
-            // And we are overwriting the global voice because of that
-            // Seems kind wrong
-            let final_voice = match msg.voice {
-                Some(voice) => {
-                    if is_global_voice_enabled {
-                        println!("Using Global Voice");
-                        global_voice.clone()
-                    } else {
-                        voice
-                    }
+        let final_voice = match &msg.voice {
+            Some(voice) => {
+                if is_global_voice_enabled {
+                    println!("Using Global Voice");
+                    global_voice
+                } else {
+                    voice.clone()
                 }
-                None => {
-                    if is_global_voice_enabled {
-                        println!("Using Global Voice");
-                        global_voice.clone()
-                    } else {
-                        match user_voice_opt {
-                            Ok(user_voice) => user_voice,
-                            Err(_) => global_voice.clone(),
-                        }
-                    }
-                }
-            };
-
-            let filename = subd_elevenlabs::twitch_chat_filename(
-                msg.username.clone(),
-                final_voice.clone(),
-            );
-
-            let chat_message =
-                subd_elevenlabs::sanitize_chat_message(msg.message.clone());
-
-            // We keep track if we choose a random name for the user,
-            // so we can inform them on screen
-            let mut _is_random = false;
-
-            let voice_data =
-                subd_elevenlabs::find_voice_id_by_name(&final_voice);
-            let (voice_id, _voice_name) = match voice_data {
-                Some((id, name)) => (id, name),
-                None => {
-                    _is_random = true;
-                    subd_elevenlabs::find_random_voice()
-                }
-            };
-
-            // The voice here in the TTS body is final
-            let tts_body = TtsBody {
-                model_id: None,
-                text: chat_message,
-                voice_settings: None,
-            };
-            println!("Calling TTS");
-            let tts_result = self.elevenlabs.tts(&tts_body, voice_id);
-            let bytes = match tts_result {
-                Ok(v) => v,
-                Err(e) => {
-                    eprintln!("ElevenLabs TTS Error: {:?}", e);
-
-                    // How do I not crash
-                    continue;
-                }
-            };
-            println!("Done Calling TTS");
-
-            // w/ Extension
-            let full_filename = format!("{}.wav", filename);
-            // TODO: Don't reference begin's computer
-            let tts_folder = "./TwitchChatTTSRecordings";
-            let mut local_audio_path =
-                format!("{}/{}", tts_folder, full_filename);
-
-            if let Err(e) = std::fs::write(local_audio_path.clone(), bytes) {
-                eprintln!("Error writing tts file: {:?}", e);
-                continue;
             }
-
-            if msg.reverb {
-                let res = subd_elevenlabs::normalize_tts_file(
-                    local_audio_path.clone(),
-                )
-                .and_then(|audio_path| {
-                    subd_elevenlabs::add_reverb(audio_path.clone())
-                });
-                if let Ok(audio_path) = res {
-                    local_audio_path = audio_path
-                };
+            None => {
+                if is_global_voice_enabled {
+                    println!("Using Global Voice");
+                    global_voice
+                } else {
+                    user_voice
+                }
             }
+        };
 
-            if let Some(stretch) = msg.stretch {
-                let res = subd_elevenlabs::normalize_tts_file(
-                    local_audio_path.clone(),
-                )
-                .and_then(|audio_path| {
-                    subd_elevenlabs::stretch_audio(audio_path, stretch)
-                });
-                if let Ok(audio_path) = res {
-                    local_audio_path = audio_path
-                };
+        Ok(final_voice)
+    }
+
+    async fn process_audio(
+        &self,
+        final_voice: &str,
+        chat_message: &str,
+        msg: &ElevenLabsRequest,
+    ) -> Result<String> {
+        let filename = subd_elevenlabs::twitch_chat_filename(
+            msg.username.clone(),
+            final_voice.to_string(),
+        );
+        let voice_data = subd_elevenlabs::find_voice_id_by_name(final_voice)
+            .unwrap_or_else(subd_elevenlabs::find_random_voice);
+        let (voice_id, _voice_name) = voice_data;
+
+        let tts_body = TtsBody {
+            model_id: None,
+            text: chat_message.to_string(),
+            voice_settings: None,
+        };
+
+        // TODO: What's up with tts here
+        println!("Calling TTS");
+
+        //            let tts_result = self.elevenlabs.tts(&tts_body, voice_id);
+        let bytes = self.elevenlabs.tts(&tts_body, voice_id).map_err(|e| {
+            eprintln!("ElevenLabs TTS Error: {:?}", e);
+            anyhow!(e)
+        })?;
+        println!("Done Calling TTS");
+
+        let full_filename = format!("{}.wav", filename);
+        let tts_folder = "./TwitchChatTTSRecordings";
+        let mut local_audio_path = format!("{}/{}", tts_folder, full_filename);
+
+        std::fs::write(&local_audio_path, bytes).map_err(|e| {
+            eprintln!("Error writing tts file: {:?}", e);
+            e
+        })?;
+
+        local_audio_path = self
+            .apply_audio_effects(local_audio_path.clone(), final_voice, msg)
+            .await?;
+
+        Ok(local_audio_path)
+    }
+
+    async fn apply_audio_effects(
+        &self,
+        audio_path: String,
+        final_voice: &str,
+        msg: &ElevenLabsRequest,
+    ) -> Result<String> {
+        let mut local_audio_path = audio_path;
+
+        if msg.reverb {
+            if let Ok(audio_path) =
+                subd_elevenlabs::normalize_tts_file(local_audio_path.clone())
+                    .and_then(|audio_path| {
+                        subd_elevenlabs::add_reverb(audio_path.clone())
+                    })
+            {
+                local_audio_path = audio_path;
             }
+        }
 
-            if let Some(pitch) = msg.pitch {
-                let res = subd_elevenlabs::normalize_tts_file(
-                    local_audio_path.clone(),
-                )
-                .and_then(|audio_path| {
-                    subd_elevenlabs::change_pitch(audio_path, pitch)
-                });
-                if let Ok(audio_path) = res {
-                    local_audio_path = audio_path
-                };
-            };
+        if let Some(stretch) = &msg.stretch {
+            if let Ok(audio_path) =
+                subd_elevenlabs::normalize_tts_file(local_audio_path.clone())
+                    .and_then(|audio_path| {
+                        subd_elevenlabs::stretch_audio(
+                            audio_path,
+                            stretch.clone(),
+                        )
+                    })
+            {
+                local_audio_path = audio_path;
+            }
+        }
 
-            if final_voice == "evil_pokimane" {
-                let res = subd_elevenlabs::normalize_tts_file(
+        if let Some(pitch) = &msg.pitch {
+            if let Ok(audio_path) =
+                subd_elevenlabs::normalize_tts_file(local_audio_path.clone())
+                    .and_then(|audio_path| {
+                        subd_elevenlabs::change_pitch(audio_path, pitch.clone())
+                    })
+            {
+                local_audio_path = audio_path;
+            }
+        }
+
+        match final_voice {
+            "evil_pokimane" => {
+                if let Ok(audio_path) = subd_elevenlabs::normalize_tts_file(
                     local_audio_path.clone(),
                 )
                 .and_then(|audio_path| {
@@ -217,14 +221,13 @@ impl EventHandler for ElevenLabsHandler {
                         "-200".to_string(),
                     )
                 })
-                .and_then(subd_elevenlabs::add_reverb);
-                if let Ok(audio_path) = res {
-                    local_audio_path = audio_path
-                };
+                .and_then(subd_elevenlabs::add_reverb)
+                {
+                    local_audio_path = audio_path;
+                }
             }
-
-            if final_voice == "satan" {
-                let res = subd_elevenlabs::normalize_tts_file(
+            "satan" => {
+                if let Ok(audio_path) = subd_elevenlabs::normalize_tts_file(
                     local_audio_path.clone(),
                 )
                 .and_then(|audio_path| {
@@ -233,46 +236,46 @@ impl EventHandler for ElevenLabsHandler {
                         "-350".to_string(),
                     )
                 })
-                .and_then(subd_elevenlabs::add_reverb);
-                if let Ok(audio_path) = res {
-                    local_audio_path = audio_path
-                };
+                .and_then(subd_elevenlabs::add_reverb)
+                {
+                    local_audio_path = audio_path;
+                }
             }
-
-            // What is the difference
-            if final_voice == "god" {
-                let res = subd_elevenlabs::normalize_tts_file(
+            "god" => {
+                if let Ok(audio_path) = subd_elevenlabs::normalize_tts_file(
                     local_audio_path.clone(),
                 )
-                .and_then(subd_elevenlabs::add_reverb);
-                if let Ok(audio_path) = res {
-                    local_audio_path = audio_path
-                };
+                .and_then(subd_elevenlabs::add_reverb)
+                {
+                    local_audio_path = audio_path;
+                }
             }
+            _ => {}
+        }
 
-            self.sink.set_volume(0.5);
-            match final_voice.as_str() {
-                "melkey" => self.sink.set_volume(1.0),
-                "beginbot" => self.sink.set_volume(1.0),
-                "evil_pokimane" => self.sink.set_volume(1.0),
-                "satan" => self.sink.set_volume(0.7),
-                "god" => self.sink.set_volume(0.7),
-                _ => {
-                    self.sink.set_volume(0.5);
-                }
-            };
-            let f = match File::open(local_audio_path) {
-                Ok(v) => v,
-                Err(e) => {
-                    eprintln!("Error opening tts file: {:?}", e);
-                    continue;
-                }
-            };
-            let file = BufReader::new(f);
-            self.sink
-                .append(Decoder::new(BufReader::new(file)).unwrap());
+        Ok(local_audio_path)
+    }
 
+    fn play_audio(&self, audio_path: &str, final_voice: &str) {
+        self.sink.set_volume(match final_voice {
+            "melkey" | "beginbot" | "evil_pokimane" => 1.0,
+            "satan" | "god" => 0.7,
+            _ => 0.5,
+        });
+
+        let file = match File::open(audio_path) {
+            Ok(f) => BufReader::new(f),
+            Err(e) => {
+                eprintln!("Error opening tts file: {:?}", e);
+                return;
+            }
+        };
+
+        if let Ok(decoder) = Decoder::new(BufReader::new(file)) {
+            self.sink.append(decoder);
             self.sink.sleep_until_end();
+        } else {
+            eprintln!("Error decoding audio file");
         }
     }
 }
