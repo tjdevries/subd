d71cffb535323afcbe78c044ee215d8085debce3
beginbot <beginbot@beginbots-MBP.home>
2024-10-03 01:02:50 -0700

    its kinda a site

--- src/bin/music_video_creator_site.rs
+++ src/bin/music_video_creator_site.rs
@@ -4,7 +4,7 @@ use axum::{
     response::Html,
     routing::{get, Router},
 };
-use std::{fs, net::SocketAddr};
+use std::fs;
 use subd_db::get_db_pool;
 use tower_http::{
     cors::{Any, CorsLayer},
@@ -23,10 +23,9 @@ async fn main() {
         .route("/", get(root))
         // Serve static files from "./tmp/fal_images" at the "/images" path
         .nest_service("/images", serve_dir)
-        // Add CORS layer if needed
         .layer(
             CorsLayer::new()
-                .allow_origin(Any) // Allow any origin
+                .allow_origin(Any)
                 .allow_methods([Method::GET]),
         );
 
@@ -34,14 +33,12 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-// Handler that responds with an HTML page displaying images in a grid
 async fn root() -> Result<Html<String>, (StatusCode, String)> {
     let pool = get_db_pool().await;
     let songs = ai_songs_vote::get_top_songs(&pool, 5)
         .await
         .map_err(|_| "Error getting top songs")
         .unwrap();
-    // Read the "./tmp/fal_images" directory
     let entries = fs::read_dir("./tmp/fal_images").map_err(|e| {
         (
             StatusCode::INTERNAL_SERVER_ERROR,
@@ -49,7 +46,6 @@ async fn root() -> Result<Html<String>, (StatusCode, String)> {
         )
     })?;
 
-    // Collect image file names
     let images = entries
         .filter_map(|entry| {
             let entry = entry.ok()?;
@@ -68,14 +64,25 @@ async fn root() -> Result<Html<String>, (StatusCode, String)> {
         })
         .collect::<Vec<_>>();
 
-    // Start building the HTML content
+    let count = ai_playlist::total_ai_songs(&pool).await.unwrap();
+
     let mut html = String::from(
         "<html>
             <head>
                 <style>
+                    body {
+                        font-family: \"Papyrus\";
+                    }
+                    .sub-header{
+                        font-size: 200%;
+                    }
+
+                    .header{
+                        font-size: 400%;
+                    }
                     .grid-container {
                         display: grid;
-                        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+                        grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
                         grid-gap: 10px;
                     }
                     .grid-item {
@@ -84,27 +91,39 @@ async fn root() -> Result<Html<String>, (StatusCode, String)> {
                 </style>
             </head>
             <body>
-                <div class=\"grid-container\">
         ",
     );
 
     let base_path = "/images";
 
-    // We need total songs
+    html.push_str(&"<h1 class=\"header grid-item\"> AI Top of the Pops</h1>");
+    html.push_str(&format!(
+        "<h2 class=\"sub-header grid-item\"> Total AI Songs Created: {}</h2>",
+        count
+    ));
+    html.push_str(&format!(
+        "<h2 class=\"sub-header grid-item\"> Total AI Song Votes: {}</h2>",
+        count
+    ));
 
-    // We need to display the top 5 songs
-    for song in songs {
-        html.push_str(&format!("Song: {}", song.title))
-    }
+    html.push_str(&"<h1 class=\"grid-item\"><code class=\"\">!vote 0.0 - 10.0</code></h1>");
+    html.push_str(&"<hr />");
+    html.push_str("<div class=\"grid-container\">");
+
+    // Soon this will be be top of the pops
+    // We would like to display the top 5 songs
+    //for song in songs {
+    //    html.push_str(&format!("Song: {}", song.title))
+    //}
 
     // Add each image and its ID to the grid
     for (index, image) in images.into_iter().enumerate() {
         html.push_str(&format!(
             "<div class=\"grid-item\">
-                <img src=\"{}/{}\" alt=\"{}\" style=\"max-width:200px; max-height:200px;\" /><br/>
-                <b>{}</b>
+                <img src=\"{}/{}\" alt=\"{}\" style=\"max-width:400px; max-height:400px;\" /><br/>
+                <h1><code>!like {} | !veto {}</code></h1>
             </div>",
-            base_path, image, image, index
+            base_path, image, image, index, index
         ));
     }
 
