67bbca3d73e67987d6a6aa2afcdb59a9c06b01cf
David Begin <davidmichaelbe@gmail.com>
2024-10-05 19:23:55 -0700

    We are trying to get structured responses from AI

--- src/bin/ai_test.rs
+++ src/bin/ai_test.rs
@@ -1,45 +1,43 @@
-// use kalosm::language::*;
+use instruct_macros::InstructMacro;
+use instruct_macros_types::Parameter;
+use instruct_macros_types::{ParameterInfo, StructInfo};
+use instructor_ai::from_openai;
+use openai_api_rs::v1::{
+    api::Client,
+    chat_completion::{self, ChatCompletionRequest},
+    common::GPT3_5_TURBO,
+};
+use serde::{Deserialize, Serialize};
+use std::env;
 
-// First, derive an efficient parser for your structured data
-// #[derive(Parse, Clone, Debug)]
-// enum Class {
-//     Thing,
-//     Person,
-//     Animal,
-// }
-//
-// #[derive(Parse, Clone, Debug)]
-// struct Response {
-//     classification: Class,
-// }
+fn main() {
+    let client = Client::new(env::var("OPENAI_API_KEY").unwrap().to_string());
+    let instructor_client = from_openai(client);
 
-#[tokio::main]
-async fn main() {
-    // let base_url = std::env::var("OPENAI_API_BASE")
-    //     .expect("Custom OPENAI_API_BASE not set");
-    // let llm = Gpt3_5::builder().with_base_url(&base_url).build();
-    //
-    // let task = Task::builder("You classify the user's message as about a person, animal or thing in a JSON format")
-    //     .with_constraints(Response::new_parser())
-    //     .build();
-    // let response = task.run("The Kalosm library lets you create structured data from natural language inputs", &llm).await.unwrap();
+    #[derive(InstructMacro, Debug, Serialize, Deserialize)]
+    // This represents a single user
+    struct UserInfo {
+        // This represents the name of the user
+        name: String,
+        // This represents the age of the user
+        age: u8,
+    }
 
-    println!("  The Kalosm library lets you create structured data from natural language inputs");
-    // println!("{:?}", response);
+    let req = ChatCompletionRequest::new(
+        GPT3_5_TURBO.to_string(),
+        vec![chat_completion::ChatCompletionMessage {
+            role: chat_completion::MessageRole::user,
+            content: chat_completion::Content::Text(String::from(
+                "John Doe is a 30 year old software engineer",
+            )),
+            name: None,
+        }],
+    );
 
-    // let prompt = "The following is a 300 word essay about why the capital of France is Paris:";
-    // print!("{}", prompt);
-    //
-    // let mut stream =
-    //     llm.stream_text(prompt).with_max_length(300).await.unwrap();
-    // stream.to_std_out().await.unwrap();
+    let result = instructor_client
+        .chat_completion::<UserInfo>(req, 3)
+        .unwrap();
 
-    // // Then set up a task with a prompt and constraints
-    // let llm = Llama::new_chat().await.unwrap();
-    // let task = Task::builder("You classify the user's message as about a person, animal or thing in a JSON format")
-    //     .with_constraints(Response::new_parser())
-    //     .build();
-    //
-    // // Finally, run the task
-    // let response = task.run("The Kalosm library lets you create structured data from natural language inputs", &llm).await.unwrap();
+    println!("{}", result.name); // John Doe
+    println!("{}", result.age); // 30
 }
