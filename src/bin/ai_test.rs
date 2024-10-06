8c33cd4332847b7c5ef3bc4ea6e3673b5b1bd213
David Begin <davidmichaelbe@gmail.com>
2024-10-05 19:26:38 -0700

    POC of structuring data from LLM

--- src/bin/ai_test.rs
+++ src/bin/ai_test.rs
@@ -10,25 +10,24 @@ use openai_api_rs::v1::{
 use serde::{Deserialize, Serialize};
 use std::env;
 
+#[derive(InstructMacro, Debug, Serialize, Deserialize)]
+struct UserInfo {
+    // This represents the name of the user
+    image_prompts: Vec<String>,
+    // This represents the age of the user
+    // age: u8,
+}
+
 fn main() {
     let client = Client::new(env::var("OPENAI_API_KEY").unwrap().to_string());
     let instructor_client = from_openai(client);
-
-    #[derive(InstructMacro, Debug, Serialize, Deserialize)]
-    // This represents a single user
-    struct UserInfo {
-        // This represents the name of the user
-        name: String,
-        // This represents the age of the user
-        age: u8,
-    }
 
     let req = ChatCompletionRequest::new(
         GPT3_5_TURBO.to_string(),
         vec![chat_completion::ChatCompletionMessage {
             role: chat_completion::MessageRole::user,
             content: chat_completion::Content::Text(String::from(
-                "John Doe is a 30 year old software engineer",
+                "Describe 5 image_prompts for a Music Video about Programming in Rust",
             )),
             name: None,
         }],
@@ -38,6 +37,7 @@ fn main() {
         .chat_completion::<UserInfo>(req, 3)
         .unwrap();
 
-    println!("{}", result.name); // John Doe
-    println!("{}", result.age); // 30
+    println!("{:?}", result);
+    // println!("{}", result.name); // John Doe
+    // println!("{}", result.age); // 30
 }
