0ca8aef1127aad4cc1710d15471f9bfa86c60f5c
David Begin <davidmichaelbe@gmail.com>
2024-10-06 20:27:30 -0700

    Trying to cleanup

--- src/bin/music_video_creator_site.rs
+++ src/bin/music_video_creator_site.rs
@@ -51,13 +51,15 @@ async fn create_app() -> Router {
         pool: Arc::new(pool),
     };
 
+    // We need to rename the routes here
+    // We also need to unify all the good collecting data from the database
     Router::new()
-        .route("/", get(root))
+        .route("/", get(home))
         .route("/ai_songs/:id", get(show_ai_song))
         .route("/users/:username", get(show_ai_song_by_user))
-        .route("/songs", get(show_ai_songs))
+        .route("/all_songs", get(show_ai_songs))
         .nest_service("/images", ServeDir::new("./tmp/music_videos"))
-        .nest_service("/all_songs", ServeDir::new("./ai_songs"))
+        .nest_service("/songs", ServeDir::new("./ai_songs"))
         .nest_service("/static", ServeDir::new("./static"))
         .with_state(state)
         .layer(
@@ -67,22 +69,31 @@ async fn create_app() -> Router {
         )
 }
 
-async fn root(
+// This needs to have all the code to grab values abstracted
+async fn home(
     State(state): State<AppState>,
 ) -> Result<Html<String>, (StatusCode, String)> {
     let pool = &state.pool;
-    let stats = fetch_stats(pool).await?;
-    let unplayed_songs = ai_playlist::get_unplayed_songs(pool).await.unwrap();
+    // TODO Get a better name here
+    // should we fail on this?
+    let stats = fetch_stats(pool)
+        .await
+        .map_err(|e| (StatusCode::BAD_REQUEST, e.to_string()))?;
+
+    let unplayed_songs = ai_playlist::get_unplayed_songs(pool)
+        .await
+        .unwrap_or(vec![]);
     let current_song = ai_playlist::get_current_song(pool).await.ok();
-    let current_song_votes_count = if let Some(song) = &current_song {
-        ai_songs_vote::total_votes_by_id(pool, song.song_id)
+
+    // This await is fine here???
+    let current_song_votes_count = match current_song.as_ref() {
+        Some(song) => ai_songs_vote::total_votes_by_id(pool, song.song_id)
             .await
-            .unwrap_or(0)
-    } else {
-        0
+            .unwrap_or(0),
+        None => 0,
     };
 
-    // Only if this is something
+    // This could be a method
     let (videos, image_scores) = if let Some(song) = &current_song {
         let music_directory = format!("./tmp/music_videos/{}/", song.song_id);
 
@@ -93,19 +104,17 @@ async fn root(
         .iter()
         .map(|path| path.to_string())
         .collect::<Vec<String>>();
-        let videos = subd_utils::get_files_by_ext(&music_directory, &["mp4"]);
-
-        // We actually need the extension
-        // let image_scores = ai_playlist::models::get_all_image_votes_for_song(
         let image_scores =
-            // ai_playlist::models::get_image_votes_or_default(pool, ids)
-            ai_playlist::models::get_image_votes_or_default_with_extensions(pool, ids)
-                .await
-                .unwrap_or_default();
-        // let image_scores = vec![];
+            ai_playlist::models::get_image_votes_or_default_with_extensions(
+                pool, ids,
+            )
+            .await
+            .unwrap_or_default();
 
+        let videos = subd_utils::get_files_by_ext(&music_directory, &["mp4"]);
         (videos, image_scores)
     } else {
+        // TODO: this feels wrong
         (vec![], vec![("".to_string(), "".to_string(), 0, 0)])
     };
 
@@ -138,7 +147,9 @@ async fn show_ai_songs(
     State(state): State<AppState>,
 ) -> Result<Html<String>, (StatusCode, String)> {
     let pool = &state.pool;
-    let stats = fetch_stats(pool).await?;
+    let stats = fetch_stats(pool)
+        .await
+        .map_err(|e| (StatusCode::BAD_REQUEST, e.to_string()))?;
 
     // This must be failing
     let songs = ai_playlist::all_songs(pool)
@@ -167,40 +178,9 @@ async fn show_ai_song_by_user(
     let songs = ai_playlist::models::get_songs_for_user(&pool, &username)
         .await
         .unwrap();
-    let stats = fetch_stats(pool).await?;
-    //let current_song = ai_playlist::find_song_by_id(pool, &id.to_string())
-    //    .await
-    //    .map_err(|_| (StatusCode::NOT_FOUND, "Song not found".to_string()))?;
-    //let current_song_votes_count =
-    //    ai_songs_vote::total_votes_by_id(pool, current_song.song_id)
-    //        .await
-    //        .unwrap_or(0);
-    //
-    //let music_directory =
-    //    format!("./tmp/music_videos/{}/", current_song.song_id);
-    //let images =
-    //    subd_utils::get_files_by_ext(&music_directory, &["png", "jpg", "jpeg"]);
-    //let videos = subd_utils::get_files_by_ext(&music_directory, &["mp4"]);
-    //
-    //let image_scores = ai_playlist::models::get_all_image_votes_for_song(
-    //    pool,
-    //    current_song.song_id,
-    //)
-    //.await
-    //.unwrap_or(vec![]);
-    //
-    //let base_path = format!("/images/{}", current_song.song_id);
-    //
-    //let context = context! {
-    //    stats,
-    //    current_song,
-    //    current_song_votes_count,
-    //    images,
-    //    videos,
-    //    image_scores,
-    //    base_path,
-    //};
-    //
+    let stats = fetch_stats(pool)
+        .await
+        .map_err(|e| (StatusCode::BAD_REQUEST, e.to_string()))?;
     let context = context! {stats, songs};
     let tmpl = ENV.get_template("songs.html").unwrap();
     //
@@ -216,7 +196,9 @@ async fn show_ai_song(
     Path(id): Path<uuid::Uuid>,
 ) -> Result<Html<String>, (StatusCode, String)> {
     let pool = &state.pool;
-    let stats = fetch_stats(pool).await?;
+    let stats = fetch_stats(pool)
+        .await
+        .map_err(|e| (StatusCode::BAD_REQUEST, e.to_string()))?;
     let current_song = ai_playlist::find_song_by_id(pool, &id.to_string())
         .await
         .map_err(|_| (StatusCode::NOT_FOUND, "Song not found".to_string()))?;
@@ -259,11 +241,11 @@ async fn show_ai_song(
     Ok(Html(body))
 }
 
-async fn fetch_stats(pool: &PgPool) -> Result<Stats, (StatusCode, String)> {
-    let ai_songs_count = ai_playlist::total_ai_songs(pool).await.unwrap();
-    let ai_votes_count = ai_songs_vote::total_votes(pool).await.unwrap();
+async fn fetch_stats(pool: &PgPool) -> Result<Stats> {
+    let ai_songs_count = ai_playlist::total_ai_songs(pool).await.unwrap_or(0);
+    let ai_votes_count = ai_songs_vote::total_votes(pool).await.unwrap_or(0);
     let unplayed_songs_count =
-        ai_playlist::count_unplayed_songs(pool).await.unwrap();
+        ai_playlist::count_unplayed_songs(pool).await.unwrap_or(0);
     Ok(Stats {
         ai_songs_count,
         ai_votes_count,
